71add4b3a6c7eea00330452a650369f06d2eb558
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-04-14 17:52:33 -0400

    modificado main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-let varDiv = document.querySelectorAll("div");
-for (elem of varDiv) {
-    elem.addEventListener("onload", function(e){
-        console.log(elem);
-    });
-    
-}
+let pintas = ['espada', 'trebol', 'corazon', 'diamante'];
+let cartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+let carta = Math.floor(Math.random()*cartas.length);
+let pinta = Math.floor(Math.random()*pintas.length);
+
+document.getElementById("numero").innerHTML = cartas[carta];
+document.getElementById("numero").classList.add(pintas[pinta]);
